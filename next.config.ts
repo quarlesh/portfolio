a457913c44740db9ef87f055dc7d41d521d3d503
Haylee Quarles <quarleshaylee@gmail.com>
2026-03-24 20:10:55 -0400

    updating PDF viewer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      canvas: false,
+    };
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false,
+    };
+
+    return config;
+  },
+  turbopack: {}
 };
 
 export default nextConfig;
